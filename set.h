3a04b0458ab6331aa98097a0cce725b26b154fc4
yarasrepo <145348472+yarasrepo@users.noreply.github.com>
2023-11-08 22:07:01 +0300

    Set implementation - yara

---
+++ set.h
@@ -0,0 +1,25 @@
+#ifndef SET_H
+#define SET_H
+
+#include <stdbool.h>
+
+typedef struct SetNode SetNode;
+typedef struct Set Set;
+
+Set* createSet();
+
+void addToSet(Set* set, const char* element);
+
+int isInSet(const Set* set, const char* element);
+
+int getSize(const Set* set);
+
+void printSet(const Set* set);
+
+void removeFromSet(Set* set, const char* element);
+
+void clearSet(Set* set);
+
+void destroySet(Set* set);
+
+#endif
