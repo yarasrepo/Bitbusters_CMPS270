db52d78da446fb2a593cc152ce7cf960b2d1c46c
yarasrepo <145348472+yarasrepo@users.noreply.github.com>
2023-11-09 00:11:44 +0300

    Add files via upload

--- driver.c
+++ driver.c
@@ -36,8 +36,7 @@ void playGame(char p1Name[], char p2Name[], CharMap* charMap){
     printf("tossing coin...\n");
     int coinToss = rand() % 2;
     char word[100];
-    printf("Player %d starts!\nChoose a word from the list to start the game\n", coinToss+1);
-    scanf("%s", word);
+    printf("Player %d starts!\n", coinToss+1);
 
     Set* usedWords= createSet();
 
@@ -45,6 +44,28 @@ void playGame(char p1Name[], char p2Name[], CharMap* charMap){
     char lastword[1000];
     
     while (i< 1000){
+        
+        printf("Player %d choose a word from the list: ", i%2 +1);
+        scanf("%s", word);
+
+        char firstChar= word[0];
+
+        if (isInCharMap(charMap, firstChar, word)==0  && i!=coinToss){
+            printf("Word is not in the list! Player %d wins!\n",(i+1)%2 +1);
+            return;
+        }
+
+        else if (isInSet(usedWords, word) == 1 && i!=coinToss){
+            printf("Word already used! Player %d wins!\n", (i+1)%2 +1);
+            return;
+        }
+
+        else if (lastword[strlen(lastword) - 1] != word[0] && i!=coinToss){
+            printf("The word does not start with the last letter of the previous word! Player %d wins\n", (i+1)%2 +1);
+            return;
+        }
+        addToSet(usedWords, word);
+
         char lastChar = word[strlen(word) - 1];
 
         if (charMap->map[idxOfKey(lastChar)] == NULL ){
@@ -52,31 +73,15 @@ void playGame(char p1Name[], char p2Name[], CharMap* charMap){
             return;
         }
 
+        printf("%s\n", word);
+        printf("%d\n", getSize(charMap->map[idxOfKey(lastChar)]));
+        removeFromSet(charMap->map[idxOfKey(lastChar)], word);
+        printSet(charMap->map[idxOfKey(lastChar)]);
+        printf("%d\n", getSize(charMap->map[idxOfKey(lastChar)]));
+
         printf("Good Spell!!");
-
-        i++;
         strcpy(lastword, word);
-
-        printf("Player %d choose a word from the list: ", i%2);
-        scanf("%s", word);
-
-        char firstChar= word[0];
-
-        if (isInCharMap(charMap, firstChar, word)==0){
-            printf("Word is not in the list! Player %d wins!\n",(i+1)%2 );
-            return;
-        }
-
-        else if (isInSet(usedWords, word) == 1){
-            printf("Word already used! Player %d wins!\n", (i+1)%2);
-            return;
-        }
-
-        else if (lastword[strlen(word) - 1] != word[0]){
-            printf("The word does not start with the last letter of the previous word! Player %d wins\n", (i+1)%2);
-            return;
-        }
-        addToSet(usedWords, word);
+        i++;
     }
 }
 
