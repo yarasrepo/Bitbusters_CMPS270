3aa7fd20b8ea64701f6e2617baa33d70adf90222
HananZ <145458776+HananZ29@users.noreply.github.com>
2023-11-11 11:36:45 +0200

    Add files via upload

---
+++ spellmaster.h
@@ -0,0 +1,12 @@
+#ifndef SPELLMASTER_H
+#define SPELLMASTER
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "set.h"
+#include "charmap.h"
+char* Mode(char lastChar, CharMap* charMap,char* mode);
+char* ModeHelper (CharMap* charMap);
+char *medium(char lastletter, CharMap *charmap);
+int compare(const void *a, const void *b);
